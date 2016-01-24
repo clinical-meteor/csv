248aa9fdfaaf85fad16c350697a488b984321213
Abigail Watson <awatson1978@gmail.com>
2016-01-24 00:41:20 -0800

    updated readme

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'clinical:csv',
-  version: '0.0.1',
+  version: '0.1.0',
   // Brief, one-line summary of the package.
   summary: 'Utilities for importing and exporting comma separated value (CSV) files.',
   // URL to the Git repository containing the source code for this package.
